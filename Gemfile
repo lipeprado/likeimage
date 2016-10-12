4b649103ea4c87ead883cdc6cb9f47ecb7244d0c
Filipe Prado <fi-lipe2010@hotmail.com>
2016-10-12 10:31:32 -0300

    Vote System

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'devise', '~> 4.2'
 gem 'haml', '~> 4.0', '>= 4.0.7'
 gem 'simple_form', '~> 3.3', '>= 3.3.1'
 gem 'paperclip', '~> 5.1'
+gem 'acts_as_votable', '~> 0.10.0'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
+    member do
+      get "curtir", to: "posts#upvote"
+      get "descurtir", to: "posts#downvote"
+    end
     resources :comments
   end
 
